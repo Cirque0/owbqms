4604a807fb9f63c5dfde1cf4787e5e4772d1dbeb
Cirque0 <62349517+Cirque0@users.noreply.github.com>
2024-02-23 19:41:10 +0800

    migrations and seeder for users and roles

--- database/migrations/2024_02_23_101959_create_user_profiles_table.php
+++ database/migrations/2024_02_23_101959_create_user_profiles_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->date('birthdate');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->unique()->constrained()->cascadeOnDelete();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('gender');
+            $table->string('contact_num');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_profiles');
     }
 };
