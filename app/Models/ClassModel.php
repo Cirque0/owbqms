3f29f0babe9a7b52c327c61f09751cbcf2c81d33
Cirque0 <62349517+Cirque0@users.noreply.github.com>
2024-02-28 19:54:46 +0800

    added models and migrations for courses, sections, subjects, classes

---
+++ app/Models/ClassModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ClassModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'classes';
+
+    protected $fillable = [
+        'section_id',
+        'subject_id',
+        'instructor_id',
+        'is_registration_open',
+    ];
+
+    public function section(): BelongsTo
+    {
+        return $this->belongsTo(Section::class);
+    }
+
+    public function subject(): BelongsTo
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function instructor(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
