16abad4d7649c6751274df2d29f55b1b55ed1038
Cirque0 <62349517+Cirque0@users.noreply.github.com>
2024-03-28 19:30:34 +0800

    fixed bug duplicating exam in student exam list

--- app/Http/Controllers/Student/StudentExamsController.php
+++ app/Http/Controllers/Student/StudentExamsController.php
@@ -10,7 +10,7 @@ use Inertia\Inertia;
 class StudentExamsController extends Controller
 {
     public function index(Request $request) {
-        $query = $request->user()->assigned_exams()
+        $examsQuery = $request->user()->assigned_exams()
             ->with([
                 'exam:id,title,type',
                 'assigned_class:id,section_id,subject_id,instructor_id' => [
@@ -29,19 +29,31 @@ class StudentExamsController extends Controller
             //     }
             // ]);
         
-        $closedClassExams = (clone $query)->where(function ($query) {
-            $query->whereNull('opened_at')
-                ->orWhereNull('closed_at');
-        })->orWhere(function ($query) {
-            $query->where('opened_at', '>', date('Y-m-d H:i:s'))
-                ->orWhere('closed_at', '<=', date('Y-m-d H:i:s'));
+        // $closedClassExams = (clone $examsQuery)->where(function ($query) {
+        //     $query->whereNull('opened_at')
+        //         ->orWhereNull('closed_at');
+        // })->orWhere(function ($query) {
+        //     $query->where('opened_at', '>', date('Y-m-d H:i:s'))
+        //         ->orWhere('closed_at', '<=', date('Y-m-d H:i:s'));
+        // })->get();
+
+        $closedClassExams = (clone $examsQuery)->where(function ($query) {
+            $query->where(function ($query) {
+                $query->whereNull('opened_at')
+                    ->orWhereNull('closed_at');
+            })->orWhere(function ($query) {
+                $query->where('opened_at', '>', date('Y-m-d H:i:s'))
+                    ->orWhere('closed_at', '<=', date('Y-m-d H:i:s'));
+            });
         })->get();
 
-        $ongoingClassExams = (clone $query)->whereNotNull('opened_at')
+        $ongoingClassExams = (clone $examsQuery)->whereNotNull('opened_at')
             ->whereNotNull('closed_at')
             ->where('opened_at', '<=', date('Y-m-d H:i:s'))
             ->where('closed_at', '>', date('Y-m-d H:i:s'))
             ->get();
+
+        error_log(json_encode($closedClassExams));
 
         return Inertia::render('Student/Exams', [
             'closedClassExams' => $closedClassExams,
