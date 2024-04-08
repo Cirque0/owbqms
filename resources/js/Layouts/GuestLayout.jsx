f978b6107174e6a617a8a7f54be789db5aedfdad
Cirque0 <62349517+Cirque0@users.noreply.github.com>
2024-04-08 20:16:34 +0800

    added guest layout

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,18 +1,13 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
-
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <main className='relative min-h-screen w-full flex'>
+                <div className="md:max-w-96 w-full shrink-0 p-4 flex flex-col bg-maroon">
+                    <img src="/images/LBQMS Logo.png" alt="OWBQMS logo" className='w-96' />
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
-            </div>
-        </div>
+                    {children}
+                </div>
+
+                <div className='md:block hidden w-full bg-[url("/images/student-banner-bg.jpg")] bg-cover bg-center'></div>
+            </main>
     );
 }
